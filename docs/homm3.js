64efda042f0249290d661ce98f9c622675b3016a
d3lure <maciasmax10@gmail.com>
2022-10-31 21:54:03 +0100

    More testing of a website

--- docs/homm3.js
+++ docs/homm3.js
@@ -180,7 +180,7 @@ load the model
 */
 async function start() {
     //load the model 
-    model = await tf.loadModel('../model/model.json')
+    model = await tf.loadModel('model/model.json')
     
     //status 
     document.getElementById('status').innerHTML = 'Model Loaded';
